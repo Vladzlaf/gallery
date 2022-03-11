aff6eb9e166bedd622303411965b6ac2f4b10bd4
Vlad <94391054+Vladzlaf@users.noreply.github.com>
2022-03-11 21:01:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,18 @@ const sliderMain = new Swiper('.slider_main', {
     breakpoints: {
         0: {
             slidesPerView: 2.5,
-            spaceBetween: 30
+            spaceBetween: 20
         },
         680: {
             slidesPerView: 3.5,
-            spaceBetween: 70
+            spaceBetween: 60
         }
     }
 })
-
 const sliderBg = new Swiper('.slider_bg', {
-    freeMode: true,
     centeredSlides: true,
     parallax: true,
-    spaceBetween: 70,
+    spaceBetween: 60,
     slidesPerView: 3.5,
 })
 
@@ -30,5 +28,11 @@ document.querySelectorAll('.slider__item').forEach(item => {
     item.addEventListener('click', event => {
         item.classList.toggle('open')
     })
+})
+
+let desc = document.querySelector('.description')
+
+sliderMain.on('slideChange', e => {
+    sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
 })
 
